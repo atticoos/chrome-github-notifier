92bad393a70e9815b6df828658b31d5ac1fe6300
Atticus White <contact@atticuswhite.com>
2014-09-25 21:15:38 -0400

    server refactor

--- server/index.js
+++ server/index.js
@@ -1,45 +1,4 @@
-var express = require('express'),
-    bodyParser = require('body-parser'),
-    http = require('http'),
-    faye = require('faye'),
-    app = express(),
-    server = http.createServer(app),
-    bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 45}),
-    router;
+var Server = require('./server'),
+    GithubServer = new Server();
 
-bayeux.attach(server);
-router = express.Router();
-
-app.use(function (req, res, next) {
-  req.headers['content-type'] = req.headers['content-type'] || 'application/json';
-  next();
-});
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-// handle webhooks
-router.route('/payload').post(function (req, res) {
-  var data = {
-    event: req.headers['x-github-event'],
-    payload: JSON.parse(req.body.payload)
-  };
-  console.log('Webhook ', data.event, data.payload.action);
-  bayeux.getClient().publish('/' + data.payload.repository.id, data);
-  res.status(200).json({message: 'received'});
-});
-
-router.route('/github/oauth').get(function (req, res) {
-  console.log('req', req.params);
-  console.log('query', req.query);
-  res.json();
-});
-
-app.use('/', router);
-server.listen(4567);
-
-bayeux.on('handshake', function (clientID) {
-  console.log('handshake', clientID);
-});
-bayeux.on('subscribe', function (a) {
-  console.log('subbed', a);
-});
+GithubServer.start();
---
+++ server/server.js
@@ -0,0 +1,66 @@
+var express = require('express'),
+    request = require('request'),
+    bodyParser = require('body-parser'),
+    http = require('http'),
+    faye = require('faye'),
+    port = 4567;
+
+
+function GithubNotifierServer () {
+  this.setupHttpServer();
+  this.setupFayeServer();
+}
+
+
+GithubNotifierServer.prototype.setupHttpServer = function () {
+  this.app = express();
+  this.app.use(bodyParser.json());
+  this.app.use(bodyParser.urlencoded({extended: false}));
+  this.app.use(function (req, res, next) {
+    req.headers['content-type'] = req.headers['content-type'] || 'application/json';
+    next();
+  });
+  this.router = express.Router();
+  this.router.route('/github/oauth').get(function (req, res) {
+    console.log('req', req.params);
+    console.log('query', req.query);
+    res.json();
+  });
+  this.router.route('/payload').post(function (req, res) {
+    var data = {
+      event: req.headers['x-github-event'],
+      payload: JSON.parse(req.body.payload)
+    };
+    console.log('Webhook ', data.event, data.payload.action, data.payload.repository.id);
+    this.fayeClient.publish('/' + data.payload.repository.id, data);
+    res.status(200).json({message: 'received'});
+  }.bind(this));
+  this.app.use('/', this.router);
+  this.httpServer = http.createServer(this.app);
+
+};
+
+
+
+GithubNotifierServer.prototype.setupFayeServer = function () {
+  this.fayeServer = new faye.NodeAdapter({
+    mount: '/faye',
+    timeout: 45
+  });
+  this.fayeServer.addExtension({
+    incoming: function (message, callback) {
+      if (message.subscription) {
+        console.log('INCOMING SUB', message.subscription);
+      }
+      callback(message);
+    }
+  });
+  this.fayeServer.attach(this.httpServer);
+  this.fayeClient = this.fayeServer.getClient();
+};
+
+GithubNotifierServer.prototype.start = function () {
+  this.httpServer.listen(port);
+}
+
+module.exports = GithubNotifierServer;
